f2e3fd1858104334e4cbfc4910d6a999be923caa
Jo-Highness <114522669+Jo-Highness@users.noreply.github.com>
2026-05-29 18:59:06 +0200

    Ignore __pycache__ and test runner script

--- run_tests.sh
+++
@@ -1,6 +0,0 @@
-set -e
-pip install --quiet --root-user-action=ignore pytest-homeassistant-custom-component
-echo "=== INSTALL DONE ==="
-python -c "import homeassistant, sys; print('HA', homeassistant.const.__version__, 'PY', sys.version)"
-echo "=== RUNNING PYTEST ==="
-pytest -p no:cacheprovider
